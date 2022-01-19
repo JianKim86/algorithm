abf195d16e3271749269ab4624a62b64d9ddcc11
JianKim <o0x0oa86@gmail.com>
2022-01-19 12:26:48 +0900

    update Merge two sorted lists

--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -1,5 +1,5 @@
 public class MergeTwoSortedLists {
-    public void test() {
+    public void run() {
         //You are given the heads of two sorted linked lists list1 and list2.
         //Merge the two lists in a one sorted list.
         // The list should be made by splicing together the nodes of the first two lists.
