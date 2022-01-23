e95244c71ab9d2b8e1901505ae14d3313109e4b2
JianKim <o0x0oa86@gmail.com>
2022-01-23 13:52:33 +0900

    update Add binary

---
+++ src/AddBinary.java
@@ -0,0 +1,19 @@
+public class AddBinary {
+    public void run() {
+        //Given two binary strings a and b, return their sum as a binary string.
+        System.out.println(solution("11", "1"));
+    }
+
+    private String solution(String a, String b) {
+        StringBuffer sum = new StringBuffer();
+        int carry = 0, indexA = a.length() - 1, indexB = b.length() - 1;
+        while (indexA >= 0 || indexB >= 0 || carry == 1) {
+            int i = indexA < 0 ? 0 : a.charAt(indexA--) - '0';
+            int j = indexB < 0 ? 0 : b.charAt(indexB--) - '0';
+            sum.insert(0, (i + j + carry) % 2);
+            carry = (i + j + carry) / 2;
+        }
+
+        return sum.toString();
+    }
+}
