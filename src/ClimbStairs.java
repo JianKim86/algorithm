004f451152dec4b675c36de362f52a4c228a7cab
JianKim <o0x0oa86@gmail.com>
2022-01-24 22:52:00 +0900

    update Climb stairs(need more check)

--- src/ClimbStairs.java
+++ src/ClimbStairs.java
@@ -2,7 +2,7 @@ public class ClimbStairs {
     public void run() {
         //You are climbing a staircase. It takes n steps to reach the top.
         //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
+        System.out.println(solution(45));
     }
 
     private int solution(int n) {
